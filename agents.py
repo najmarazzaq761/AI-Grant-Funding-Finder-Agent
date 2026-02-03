0be9e07b3b47b0ebcaeb902186811f77475b13cd
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2026-02-04 00:23:27 +0500

    Update LLM model to llama-3.1-8b-instant

--- agents.py
+++ agents.py
@@ -8,7 +8,7 @@ load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 llm=LLM(
-        model="meta-llama/llama-4-maverick-17b-128e-instruct",
+        model="meta-llama/llama-3.1-8b-instant",
         base_url="https://api.groq.com/openai/v1",
         api_key=GROQ_API_KEY
     )
