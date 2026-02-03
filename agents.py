8803bd785f6fd22ecc8c7d7a9bf90bcd210fbc4b
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2026-02-04 00:27:06 +0500

    Update LLM
    Replaced LLM instance with ChatGoogleGenerativeAI for model updates.

--- agents.py
+++ agents.py
@@ -7,12 +7,13 @@ load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-llm=LLM(
-        model="meta-llama/llama-3.1-8b-instant",
-        base_url="https://api.groq.com/openai/v1",
-        api_key=GROQ_API_KEY
-    )
-
+# llm=LLM(
+#         model="meta-llama/llama-3.1-8b-instant",
+#         base_url="https://api.groq.com/openai/v1",
+#         api_key=GROQ_API_KEY
+#     )
+llm =ChatGoogleGenerativeAI(model = "gemini-2.5-flash",
+ api_key=os.getenv("GEMINI_API_KEY"))
 
 # Agent 1: Data Fetching
 data_fetch_expert = Agent(
