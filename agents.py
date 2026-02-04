c691a43873a8e024ddf2805f94f77b577febfd12
Najma Razzaq <148422805+najmarazzaq761@users.noreply.github.com>
2026-02-04 09:53:30 +0500

    Change LLM
    Updated LLM initialization to use 'groq/llama-3.1-8b-instant' model with a temperature setting.

--- agents.py
+++ agents.py
@@ -9,12 +9,15 @@ load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-llm=LLM(
-        model="meta-llama/llama-3.1-8b-instant",
-        base_url="https://api.groq.com/openai/v1",
-        api_key=GROQ_API_KEY
-    )
-# llm = ChatGroq(model="qwen/qwen3-32b",  api_key=os.getenv("GROQ_API_KEY"))
+# llm=LLM(
+#         model="meta-llama/llama-3.1-8b-instant",
+#         base_url="https://api.groq.com/openai/v1",
+#         api_key=GROQ_API_KEY
+#     )
+llm = LLM(
+    model="groq/llama-3.1-8b-instant",
+    temperature=0.5
+)
 
 # Agent 1: Data Fetching
 data_fetch_expert = Agent(
